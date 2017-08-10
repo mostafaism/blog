531b0805ea9d374eae5e7092f4d65c3492e4b9b1
Mostafa <me_psign@hotmail.com>
2017-08-10 07:06:27 +0200

    Add basic authentication

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+	
+  http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
   
   def create
   	@article = Article.find(params[:article_id])
@@ -12,7 +14,7 @@ class CommentsController < ApplicationController
     @comment.destroy
     redirect_to article_path(@article)
   end
-  
+
   private
   def comment_params
   	params.require(:comment).permit(:commenter, :body)
